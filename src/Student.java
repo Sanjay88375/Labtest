2725cfd9478c94532e9b108062c562150f923e19
Sanjay88375 <110672235+Sanjay88375@users.noreply.github.com>
2022-09-17 20:39:22 +0530

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class student            //class student
+{
+	int id;
+	String name;
+	int age;
+    double marks;
+	public student(int id,String name,int age, double marks)   //constructor of student
+	{
+		this.id=id;
+		this.name=name;
+		this.age=age;
+		this.marks=marks;
+	}
+}
